2284604def140fbd64f332fd0a3e12d7e1410bec
Luke <luchenqun@qq.com>
2024-07-14 18:17:28 +0800

    chore: update localup.sh

--- deployment/localup/localup.sh
+++ deployment/localup/localup.sh
@@ -184,7 +184,7 @@ function generate_genesis() {
 		sed -i -e "s/snapshot-interval = 0/snapshot-interval = ${SNAPSHOT_INTERVAL}/g" ${workspace}/.local/validator${i}/config/app.toml
 		sed -i -e "s/snapshot-keep-recent = 2/snapshot-keep-recent = ${SNAPSHOT_KEEP_RECENT}/g" ${workspace}/.local/validator${i}/config/app.toml
 		sed -i -e "s/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g" ${workspace}/.local/validator${i}/config/app.toml
-		sed -i -e "s/eth,net,web3/eth,txpool,personal,net,debug,web3/g" ${workspace}/.local/validator${i}/config/config.toml
+		sed -i -e "s/eth,net,web3/eth,txpool,personal,net,debug,web3/g" ${workspace}/.local/validator${i}/config/app.toml
 		sed -i -e "s/\"reserve_time\": \"15552000\"/\"reserve_time\": \"60\"/g" ${workspace}/.local/validator${i}/config/genesis.json
 		sed -i -e "s/\"forced_settle_time\": \"86400\"/\"forced_settle_time\": \"30\"/g" ${workspace}/.local/validator${i}/config/genesis.json
 		sed -i -e "s/172800s/${DEPOSIT_VOTE_PERIOD}/g" ${workspace}/.local/validator${i}/config/genesis.json
