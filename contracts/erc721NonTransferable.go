f81ba6c6b60e6ee53a6ca0ecfd36cbec9e6df160
Athas <wanghao@zkme.xyz>
2024-08-12 18:22:39 +0800

    feat(storage): add system contract for bucket NFT & group NFT

--- contracts/erc721NonTransferable.go
+++ contracts/erc721NonTransferable.go
@@ -17,16 +17,32 @@ var (
 	// ERC721NonTransferableContract is the compiled erc721 contract
 	ERC721NonTransferableContract evmtypes.CompiledContract
 
-	// ERC721NonTransferableAddress is the erc721 module address
-	ERC721NonTransferableAddress common.Address
+	// ObjectERC721TokenAddress is the object erc721 module address
+	ObjectERC721TokenAddress common.Address
+
+	// BucketERC721TokenAddress is the bucket erc721 module address
+	BucketERC721TokenAddress common.Address
+
+	// GroupERC721TokenAddress is the group erc721 module address
+	GroupERC721TokenAddress common.Address
 
 	// ObjectControlHubAddress is the object nft control hub address
 	ObjectControlHubAddress common.Address
+
+	// BucketControlHubAddress is the bucket nft control hub address
+	BucketControlHubAddress common.Address
+
+	// GroupControlHubAddress is the group nft control hub address
+	GroupControlHubAddress common.Address
 )
 
 func init() {
-	ERC721NonTransferableAddress = common.HexToAddress("0x0000000000000000000000000000000000003000")
+	ObjectERC721TokenAddress = common.HexToAddress("0x0000000000000000000000000000000000003000")
+	BucketERC721TokenAddress = common.HexToAddress("0x0000000000000000000000000000000000003001")
+	GroupERC721TokenAddress = common.HexToAddress("0x0000000000000000000000000000000000003002")
 	ObjectControlHubAddress = common.HexToAddress("0x00000Be6819f41400225702D32d3dd23663Dd690")
+	GroupControlHubAddress = common.HexToAddress("0x00000Be6819f41400225702D32d3dd23663Dd690")
+	BucketControlHubAddress = common.HexToAddress("0x00000Be6819f41400225702D32d3dd23663Dd690")
 
 	err := json.Unmarshal(ERC721NonTransferableJSON, &ERC721NonTransferableContract)
 	if err != nil {
