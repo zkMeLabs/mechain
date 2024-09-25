5e75bec4528bd9d4ae182073bfe84e2e4527375c
phenix3443 <phenix3443@gmail.com>
2024-09-25 18:54:48 +0800

    feat: update

--- scripts/create_validator_proposal.sh
+++ scripts/create_validator_proposal.sh
@@ -7,7 +7,7 @@ OUTPUT_FILE="$SCRIPT_DIR/create_validator_proposal.json"
 
 # 定义 mechaind 变量
 MECHAIND_CMD="build/mechaind"
-CONFIG_PATH="manual/"
+CONFIG_PATH="manual"
 
 # 生成密钥
 echo "add keys..."
@@ -24,7 +24,7 @@ DELEGATOR_ADDR=$($MECHAIND_CMD keys show delegator -a --keyring-backend test --h
 RELAYER_ADDR=$($MECHAIND_CMD keys show validator_relayer -a --keyring-backend test --home "$CONFIG_PATH")
 CHALLENGER_ADDR=$($MECHAIND_CMD keys show validator_challenger -a --keyring-backend test --home "$CONFIG_PATH")
 VALIDATOR_BLS=$($MECHAIND_CMD keys show validator_bls --keyring-backend test --home "$CONFIG_PATH" --output json | jq -r '.pubkey_hex')
-VALIDATOR_BLS_PROOF=$($MECHAIND_CMD tx sign ${VALIDATOR_BLS} --keyring-backend test --home "$CONFIG_PATH" --from validator_bls --output json | jq -r '.signature')
+VALIDATOR_BLS_PROOF=$($MECHAIND_CMD keys sign ${VALIDATOR_BLS} --keyring-backend test --home "$CONFIG_PATH" --from validator_bls)
 VALIDATOR_NODE_PUB_KEY=$(cat ${CONFIG_PATH}/config/priv_validator_key.json | jq -r '.pub_key.value')
 
 if [ -z "$VALIDATOR_ADDR" ] || [ -z "$DELEGATOR_ADDR" ] || [ -z "$RELAYER_ADDR" ] || [ -z "$CHALLENGER_ADDR" ] || [ -z "$VALIDATOR_BLS" ] || [ -z "$VALIDATOR_BLS_PROOF" ] || [ -z "$VALIDATOR_NODE_PUB_KEY" ]; then
@@ -32,19 +32,26 @@ if [ -z "$VALIDATOR_ADDR" ] || [ -z "$DELEGATOR_ADDR" ] || [ -z "$RELAYER_ADDR" 
     exit 1
 fi
 
-# echo "generated validator proposal file..."
-# sed -e "s|\${NODE_NAME}|$NODE_NAME|g" \
-#     -e "s|\${VALIDATOR_NODE_PUB_KEY}|$VALIDATOR_NODE_PUB_KEY|g" \
-#     -e "s|\${VALIDATOR_ADDR}|$VALIDATOR_ADDR|g" \
-#     -e "s|\${DELEGATOR_ADDR}|$DELEGATOR_ADDR|g" \
-#     -e "s|\${VALIDATOR_BLS}|$VALIDATOR_BLS|g" \
-#     -e "s|\${VALIDATOR_BLS_PROOF}|$VALIDATOR_BLS_PROOF|g" \
-#     -e "s|\${RELAYER_ADDR}|$RELAYER_ADDR|g" \
-#     -e "s|\${CHALLENGER_ADDR}|$CHALLENGER_ADDR|g" \
-#     "$TEMPLATE_FILE" >"$OUTPUT_FILE"
+echo "generated validator proposal file..."
+sed -e "s|\${NODE_NAME}|$NODE_NAME|g" \
+    -e "s|\${VALIDATOR_NODE_PUB_KEY}|$VALIDATOR_NODE_PUB_KEY|g" \
+    -e "s|\${VALIDATOR_ADDR}|$VALIDATOR_ADDR|g" \
+    -e "s|\${DELEGATOR_ADDR}|$DELEGATOR_ADDR|g" \
+    -e "s|\${VALIDATOR_BLS}|$VALIDATOR_BLS|g" \
+    -e "s|\${VALIDATOR_BLS_PROOF}|$VALIDATOR_BLS_PROOF|g" \
+    -e "s|\${RELAYER_ADDR}|$RELAYER_ADDR|g" \
+    -e "s|\${CHALLENGER_ADDR}|$CHALLENGER_ADDR|g" \
+    "$TEMPLATE_FILE" >"$OUTPUT_FILE"
 
 # if [ $? -eq 0 ]; then
 #     echo "create_validator_proposal.json has been generated successfully at $OUTPUT_FILE."
 # else
 #     echo "Error: Failed to create create_validator_proposal.json."
 # fi
+
+# $MECHAIND_CMD tx staking create-validator proposal.json --keyring-backend test --chain-id $CHAIN_ID --from ${DELEGATOR_ADDR} --node tcp://localhost:26657 -b sync --gas "200000000" --fees "100000000000000000000azkme" --yes
+
+# size=4
+# for ((i = 0; i < ${size}; i++)); do
+#     mechaind tx gov vote 1 yes --from=validator0 --chain-id=$CHAIN_ID --keyring-backend=test --gas-prices=10000azkme -y
+# done
