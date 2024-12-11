173778e178404233336748a671ecb339c04ef18a
Luke <luchenqun@qq.com>
2024-12-11 22:22:41 +0800

    feat: add solidity/verify.js for blockscout generate abi and sol file

--- solidity/contracts/storage/IStorage.sol
+++ solidity/contracts/storage/IStorage.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.0;
 
 import "../common/Types.sol";
-import "../virtualgroup/Types.sol";
 // VisibilityType is the resources public status.
 enum VisibilityType {
     UnSpecified,
